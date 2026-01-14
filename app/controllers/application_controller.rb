b00e512dba579d0708b8f17eca050aae7cba4f02
Bonv-dev <bon.v.software@gmail.com>
2026-01-14 19:18:27 +0900

    ユーザー認証：Devise を使う sign_up 時に、Devise 以外のカラムも設定する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:enabled, :name, :is_admin, :is_librarian]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+  end
 end
