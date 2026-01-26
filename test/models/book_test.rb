2c24b648be10ba36d9602b23ae4f2d185d4c72d3
Bonv-dev <bon.v.software@gmail.com>
2026-01-26 17:46:33 +0900

    自動テスト: モデルテスト追加

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = categories(:one)
+    @step = steps(:one)
+  end
+
+  test "valid with title" do
+    book = Book.new(
+      enabled: true,
+      name: "Rails Guide",
+      isbn: "978-4109999999",
+      category: @category,
+      step: @step
+    )
+    assert book.valid?
+  end
+
+  test "invalid with title" do
+    book = Book.new(
+      enabled: true,
+      name: nil,
+      isbn: "978-4109999999",
+      category: @category,
+      step: @step
+    )
+    assert book.invalid?
+  end
 end
