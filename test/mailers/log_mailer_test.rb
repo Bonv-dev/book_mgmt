59912b0b5bb4c0444947dd29164f9f8574810718
Bonv-dev <bon.v.software@gmail.com>
2026-01-26 19:46:20 +0900

    自動テスト: メールテスト追加

--- test/mailers/log_mailer_test.rb
+++ test/mailers/log_mailer_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class LogMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "change_status_email" do
+    new_log  = logs(:new_log)
+    old_log  = logs(:old_log)
+    from_user = users(:librarian)
+    to_users  = [users(:one), users(:two)]
+
+    mail = LogMailer.change_status_email(new_log, old_log, from_user, to_users)
+
+    assert_equal "蔵書のステータス変更", mail.subject
+    assert_equal [from_user.email], mail.from
+    assert_equal to_users.map(&:email), mail.to.sort
+
+    body = mail.text_part.body.to_s
+    assert_includes body, "司書名：#{from_user.name}"
+    assert_includes body, "蔵書名：#{new_log.book.name}"
+    assert_includes body, "変更"
+  end
 end
--- test/mailers/previews/log_mailer_preview.rb
+++ test/mailers/previews/log_mailer_preview.rb
@@ -1,3 +1,11 @@
 # Preview all emails at http://localhost:3000/rails/mailers/log_mailer
 class LogMailerPreview < ActionMailer::Preview
+  def change_status_email
+    new_log   = Log.where(book_id: 1, status: 2).first
+    old_log   = Log.where(book_id: 1, status: 1).first
+    from_user = User.where(is_librarian: true).first
+    to_users  = User.limit(2)
+
+    LogMailer.change_status_email(new_log, old_log, from_user, to_users)
+  end
 end
